2afa35b4a315fcfaa2e38538969cb8550f1d6b08
Cole Thorpen <thorpencole@gmail.com>
2024-02-28 18:50:34 -0800

    set up tests (6/10)

---
+++ jupyter_workflow/tests/test_data.py
@@ -0,0 +1,10 @@
+from jupyter_workflow.data import get_fremont_data
+import pandas as pd
+import numpy as np
+
+def test_fremont_data():
+    data = get_fremont_data()
+    assert all(data.columns == ['Total', 'Northbound', 'Southbound'])
+    assert isinstance(data.index, pd.DateTimeIndex)
+    assert len(np.unique(data.index.time) == 24)
+    
